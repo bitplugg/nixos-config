ba1ca950cd0496fb25a352896143c138dbaa4d53
bitplugg <zxckillka.off@gmail.com>
2026-05-08 03:37:19 +0300

    upd

--- pkgs/brrtfetch/default.nix
+++ pkgs/brrtfetch/default.nix
@@ -1,16 +1,16 @@
 { lib, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "brrtfetch";
-  version = "unstable-2024-11-24";
+  version = "latest";
 
   src = fetchFromGitHub {
     owner = "ferrebarrat";
     repo = "brrtfetch";
-    rev = "c1f327999cd3dd4dde163d183a699c73979c0fa5";
-    hash = "sha256-P6oEAL7pXGjKf6D+/IIuZgwjP2NHWeh6qx28ac1Y8Go=";
+    rev = "master";
+    hash = lib.fakeSha256;   # первая попытка — ошибка, Nix подставит правильный
   };
 
-  vendorHash = "sha256-pQpattmS9VmO3ZIh4gkkYj2DXR3qYyYAc3Y4h/gM8K4=";
+  vendorHash = lib.fakeSha256;  # то же самое
 
   meta = {
     description = "Render animated ASCII art from a GIF for your sysinfo fetcher of choice";
