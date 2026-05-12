54e3a835decfe381d48394db77839118546ccd84
bitplugg <zxckillka.off@gmail.com>
2026-05-12 17:47:33 +0300

    NEW: upd tuna

--- pkgs/tuna/default.nix
+++ pkgs/tuna/default.nix
@@ -1,27 +1,28 @@
 { pkgs, lib }:
 
 let
-  tuna-bin = pkgs.stdenv.mkDerivation rec {
-    pname = "tuna-bin";
-    version = "0.19";
-    src = pkgs.fetchurl {
-      url = "https://releases.tuna.am/tuna/${version}/tuna_${version}-1_amd64.deb";
-      hash = "";   # после первой сборки замените на реальный хеш
-    };
-    nativeBuildInputs = [ pkgs.dpkg ];
-    unpackPhase = "dpkg-deb -x $src .";
-    installPhase = ''
-      mkdir -p $out/bin
-      cp usr/local/bin/tuna $out/bin/
-    '';
-  };
+  version = "0.19";
+  arch = "amd64";
 in
 pkgs.buildFHSEnv {
   name = "tuna";
   targetPkgs = pkgs: with pkgs; [ zlib glib ];
   runScript = "tuna";
   profile = ''
-    export PATH="${tuna-bin}/bin:$PATH"
+    export PATH="${pkgs.stdenv.mkDerivation {
+      pname = "tuna-bin";
+      inherit version;
+      src = pkgs.fetchurl {
+        url = "https://releases.tuna.am/tuna/${version}/tuna_linux_${arch}.tar.gz";
+        hash = "";   # при первой сборке Nix вычислит правильный хеш
+      };
+      unpackPhase = "tar xzf $src";
+      installPhase = ''
+        mkdir -p $out/bin
+        cp tuna $out/bin/
+        chmod +x $out/bin/tuna
+      '';
+    }}/bin:$PATH"
   '';
   meta = with lib; {
     description = "Российский аналог ngrok для создания защищенных туннелей";
