d69a4e195560f7785a876bb3a372657a21d30bbe
bitplugg <zxckillka.off@gmail.com>
2026-05-12 18:13:12 +0300

    NEW: add yazi

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -14,7 +14,7 @@
     powertop
     rkn-block-checker
     inputs.antigravity-nix.packages.${pkgs.system}.default
-    tuna
+#    tuna
   ];  #++ [ self.packages.${pkgs.system}.brrtfetch ];
 
   services = {
