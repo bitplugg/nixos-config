4ee55176ac95bf2beecc4cda7aeac66b31884dcb
bitplugg <zxckillka.off@gmail.com>
2026-05-12 17:12:17 +0300

    NEW: add tuna

--- pkgs/tuna/default.nix
+++ pkgs/tuna/default.nix
@@ -1,28 +1,26 @@
-{ stdenv, dpkg, fetchurl }:
+{ stdenv, buildFHSUserEnv, fetchurl, makeWrapper }:
 
-stdenv.mkDerivation rec {
-  pname = "tuna";
-  version = "0.19";
-
-  src = fetchurl {
-    url = "https://releases.tuna.am/tuna/${version}/tuna_${version}-1_amd64.deb";
-    hash = lib.fakeSha256; # Замените на реальный хеш
+let
+  tuna-bin = stdenv.mkDerivation rec {
+    pname = "tuna-bin";
+    version = "0.19";
+    src = fetchurl {
+      url = "https://releases.tuna.am/tuna/${version}/tuna_${version}-1_amd64.deb";
+      sha256 = lib.fakeSha256;
+    };
+    nativeBuildInputs = [ dpkg makeWrapper ];
+    unpackPhase = "dpkg-deb -x $src .";
+    installPhase = ''
+      mkdir -p $out/bin
+      cp usr/local/bin/tuna $out/bin/
+    '';
   };
-
-  nativeBuildInputs = [ dpkg ];
-
-  unpackPhase = "dpkg-deb -x $src .";
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r usr/local/bin/* $out/bin/
-    chmod +x $out/bin/tuna
+in
+buildFHSUserEnv {
+  name = "tuna-env";
+  targetPkgs = pkgs: with pkgs; [ /* нужные библиотеки */ ];
+  runScript = "tuna";
+  profile = ''
+    export PATH=${tuna-bin}/bin:$PATH
   '';
-
-  meta = with lib; {
-    description = "Российский аналог ngrok для создания защищенных туннелей";
-    homepage = "https://tuna.am";
-    license = licenses.unfree;
-    platforms = platforms.linux;
-  };
 }
