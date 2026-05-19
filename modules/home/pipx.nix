3448d78cf85d4da110017233f9e2ce2259c0e11a
bitplugg <zxckillka.off@gmail.com>
2026-05-19 19:44:50 +0300

    NEW: Install requests via pipx

--- modules/home/pipx.nix
+++ modules/home/pipx.nix
@@ -5,7 +5,11 @@
     pipx  # Утилита для установки Python-приложений в изолированные окружения
     aider-chat
     python3Packages.beautifulsoup4
-    python3Packages.requests
     python3Packages.debugpy # для nvim-dap-python
+  ];
+
+  # Установка Python-пакетов через pipx
+  home.activation.installPipxPackages = [
+    { name = "requests"; package = "requests"; }
   ];
 }
