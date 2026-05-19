f261012d2b01cb2ba452dd6420f7c48fbe85319d
bitplugg <zxckillka.off@gmail.com>
2026-05-19 20:15:50 +0300

    upd

--- modules/home/kew.nix
+++ modules/home/kew.nix
@@ -1,9 +1,15 @@
-# kew - консольный музыкальный плеер
+# kew - консольный музыкальный плеер + kew-video
 { config, pkgs, ... }:
 
-{
+let
+  kew-video = pkgs.writeShellScriptBin "kew-video" ''
+    #!${pkgs.runtimeShell}
+    exec ${pkgs.python3}/bin/python3 ${./scripts/kew-video} "$@"
+  '';
+in {
   home.packages = with pkgs; [
     kew
+    kew-video  # Добавляем kew-video как команду
   ];
 
   # Настройка горячих клавиш (опционально)
@@ -12,6 +18,13 @@
     extraConfig = ''
       # Пример: запуск kew по Ctrl+Alt+M
       control + alt + m = "exec kew";
+      # Запуск kew-video по Ctrl+Alt+V
+      control + alt + v = "exec kew-video --playlist";
     '';
+  };
+
+  # Alias для kew (запуск kew-video из kew)
+  home.aliases = {
+    "kew-video" = "kew-video";
   };
 }
